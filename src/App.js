b8415e6821bfdc5294a9728d453e81c887fcf16e
Pnikanti <patrick.nikanti@gmail.com>
2024-11-10 08:56:15 +0200

    Import tailwind / dependencies. Implement placeholder start page. Implement required configurations.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { ArrowRightCircleIcon } from '@heroicons/react/24/solid'
 
 const App = () => {
-  return <div>Hello, world!</div>;
+    return (
+        <>
+        <div class="h-screen flex items-center justify-center bg-blue-500 text-white">
+            <div class="text-center flex flex-row max-w justify-center items-center">
+                <ArrowRightCircleIcon className="size-16 text-white-500"/>
+                <h1 class="text-3xl font-bold ml-4">Press Enter to continue</h1>
+            </div>
+        </div>
+        </>
+    )
 };
 
 export default App;
